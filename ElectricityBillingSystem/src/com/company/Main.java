d0d3a299fcc67e5a855c99194d99b113b3908577
AppazovYerzhan <80280211+AppazovYerzhan@users.noreply.github.com>
2021-11-13 17:02:50 +0600

    Last commit

--- ElectricityBillingSystem/src/com/company/Main.java
+++ ElectricityBillingSystem/src/com/company/Main.java
@@ -1,5 +1,16 @@
 package com.company;
 
-public class Main {
+import java.sql.SQLException;
 
+public class Main {
+    public static void main(String args[]) {
+        //creation of object "driver" of class "Driver" to implement its method "start()"
+        Driver driver = new Driver();
+        try {
+            //running the method start()
+            driver.start();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
